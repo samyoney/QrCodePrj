43de21cba6f0b8b93596d6c5323797488a575db4
yoneyama1459@gmail.com <SAM>
2024-11-18 21:41:21 +0900

    Initial Commit

---
+++ QrCodePrj/QrCodePrjApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrCodePrjApp.swift
+//  QrCodePrj
+//
+//  Created by HS-Macbook on R 6/11/18.
+//
+
+import SwiftUI
+
+@main
+struct QrCodePrjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
